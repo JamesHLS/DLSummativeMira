3e3dcc2236913b08ed3ebef5e8c769d2ae4fe4a9
JamesHLS <jameslshui@gmail.com>
2024-01-29 11:05:31 +0000

    optuna params

--- classifier.py
+++ classifier.py
@@ -27,10 +27,10 @@ def cycle(iterable):
 class_names = ['apple','aquarium_fish','baby','bear','beaver','bed','bee','beetle','bicycle','bottle','bowl','boy','bridge','bus','butterfly','camel','can','castle','caterpillar','cattle','chair','chimpanzee','clock','cloud','cockroach','couch','crab','crocodile','cup','dinosaur','dolphin','elephant','flatfish','forest','fox','girl','hamster','house','kangaroo','computer_keyboard','lamp','lawn_mower','leopard','lion','lizard','lobster','man','maple_tree','motorcycle','mountain','mouse','mushroom','oak_tree','orange','orchid','otter','palm_tree','pear','pickup_truck','pine_tree','plain','plate','poppy','porcupine','possum','rabbit','raccoon','ray','road','rocket','rose','sea','seal','shark','shrew','skunk','skyscraper','snail','snake','spider','squirrel','streetcar','sunflower','sweet_pepper','table','tank','telephone','television','tiger','tractor','train','trout','tulip','turtle','wardrobe','whale','willow_tree','wolf','woman','worm',]
 
 transform = transforms.Compose([
-    # transforms.RandomCrop(size=(32, 32), padding=4),
-    # transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomCrop(size=(32, 32), padding=4),
+    transforms.RandomHorizontalFlip(p=0.5),
     transforms.ToTensor(),
-    # transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
 ])
 
 train_loader = torch.utils.data.DataLoader(
@@ -117,10 +117,12 @@ class SimpleResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
+    
 
 
 
-N = SimpleResNet(block=SimpleResNetBlock, layers=[2, 2]).to(device)
+
+N = SimpleResNet(block=SimpleResNetBlock, layers=[3, 3]).to(device)
 
 # print the number of parameters - this should be included in your report
 print(f'> Number of parameters {len(torch.nn.utils.parameters_to_vector(N.parameters()))}')
@@ -129,7 +131,7 @@ if len(torch.nn.utils.parameters_to_vector(N.parameters())) > 100000:
     print("> Warning: you have gone over your parameter budget and will have a grade penalty!")
 
 # initialise the optimiser
-optimiser = torch.optim.Adam(N.parameters(), lr=0.017118313)
+optimiser = torch.optim.Adam(N.parameters(), lr=0.0051176762879)
 plot_data = []
 steps = 0
 
@@ -185,6 +187,7 @@ while (steps < 10000):
     plt.legend(loc="upper left")
     plt.show()
     disp.clear_output(wait=True)
+
 
 # %% [markdown]
 # **Inference on dataset**
