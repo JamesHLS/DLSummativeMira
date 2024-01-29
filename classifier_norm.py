23ce9ec9b6253d851adc426eee711eab515f81c8
JamesHLS <jameslshui@gmail.com>
2024-01-29 14:20:22 +0000

    fix: normalise inplace

---
+++ classifier_norm.py
@@ -0,0 +1,240 @@
+# %% [markdown]
+# **Main imports**
+
+# %%
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision
+import matplotlib.pyplot as plt
+from IPython import display as disp
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# %% [markdown]
+# **Import dataset**
+
+# %%
+# helper function to make getting another batch of data easier
+def cycle(iterable):
+    while True:
+        for x in iterable:
+            yield x
+
+class_names = ['apple','aquarium_fish','baby','bear','beaver','bed','bee','beetle','bicycle','bottle','bowl','boy','bridge','bus','butterfly','camel','can','castle','caterpillar','cattle','chair','chimpanzee','clock','cloud','cockroach','couch','crab','crocodile','cup','dinosaur','dolphin','elephant','flatfish','forest','fox','girl','hamster','house','kangaroo','computer_keyboard','lamp','lawn_mower','leopard','lion','lizard','lobster','man','maple_tree','motorcycle','mountain','mouse','mushroom','oak_tree','orange','orchid','otter','palm_tree','pear','pickup_truck','pine_tree','plain','plate','poppy','porcupine','possum','rabbit','raccoon','ray','road','rocket','rose','sea','seal','shark','shrew','skunk','skyscraper','snail','snake','spider','squirrel','streetcar','sunflower','sweet_pepper','table','tank','telephone','television','tiger','tractor','train','trout','tulip','turtle','wardrobe','whale','willow_tree','wolf','woman','worm',]
+
+
+transform = transforms.Compose([
+    transforms.RandomCrop(size=(32, 32), padding=4, padding_mode='reflect'),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5070937, 0.48655552, 0.44092253), std=(0.26733243, 0.256427, 0.27613324), inplace=True),
+])
+
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.CIFAR100('data', train=True, download=True, transform=transform),
+    batch_size=64, shuffle=True, num_workers=2, pin_memory=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.CIFAR100('data', 
+                                  train=False, 
+                                  download=True, 
+                                  transform=transforms.Compose([transforms.ToTensor(),
+                                                                transforms.Normalize(mean=(0.5070937, 0.48655552, 0.44092253), 
+                                                                                        std=(0.26733243, 0.256427, 0.27613324), 
+                                                                                            inplace=True)])),
+    batch_size=64, num_workers=2, pin_memory=True)
+
+train_iterator = iter(cycle(train_loader))
+test_iterator = iter(cycle(test_loader))
+
+print(f'> Size of training dataset {len(train_loader.dataset)}')
+print(f'> Size of test dataset {len(test_loader.dataset)}')
+
+
+
+# %% [markdown]
+# **View some of the test dataset**
+
+
+
+
+# %% [markdown]
+# **Define a simple model**
+
+# %%
+
+
+# https://github.com/NERSC/pytorch-examples/blob/main/models/resnet_cifar10.py
+class SimpleResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(SimpleResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+class SimpleResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=100):
+        super(SimpleResNet, self).__init__()
+        self.in_channels = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.linear = nn.Linear(32, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = torch.nn.functional.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
+
+
+
+
+N = SimpleResNet(block=SimpleResNetBlock, layers=[3, 3]).to(device)
+
+# print the number of parameters - this should be included in your report
+print(f'> Number of parameters {len(torch.nn.utils.parameters_to_vector(N.parameters()))}')
+
+if len(torch.nn.utils.parameters_to_vector(N.parameters())) > 100000:
+    print("> Warning: you have gone over your parameter budget and will have a grade penalty!")
+
+# initialise the optimiser
+optimiser = torch.optim.Adam(N.parameters(), lr=0.0051176762879)
+plot_data = []
+steps = 0
+
+# %% [markdown]
+# **Main training and testing loop**
+
+# %%
+# keep within our optimisation step budget
+while (steps < 10000):
+
+    # arrays for metrics
+    train_loss_arr = np.zeros(0)
+    train_acc_arr = np.zeros(0)
+    test_acc_arr = np.zeros(0)
+
+    # iterate through some of the train dateset
+    for i in range(1000):
+        x,t = next(train_iterator)
+        x,t = x.to(device), t.to(device)
+
+        optimiser.zero_grad()
+        p = N(x)
+        pred = p.argmax(dim=1, keepdim=True)
+        loss = torch.nn.functional.cross_entropy(p, t)
+        loss.backward()
+        optimiser.step()
+        steps += 1
+
+        train_loss_arr = np.append(train_loss_arr, loss.cpu().data)
+        train_acc_arr = np.append(train_acc_arr, pred.data.eq(t.view_as(pred)).float().mean().item())
+
+    # iterate over the entire test dataset
+    for x,t in test_loader:
+        x,t = x.to(device), t.to(device)
+        p = N(x)
+        loss = torch.nn.functional.cross_entropy(p, t)
+        pred = p.argmax(dim=1, keepdim=True)
+        test_acc_arr = np.append(test_acc_arr, pred.data.eq(t.view_as(pred)).float().mean().item())
+
+    # print your loss and accuracy data - include this in the final report
+    print('steps: {:.2f}, train loss: {:.3f}, train acc: {:.3f}±{:.3f}, test acc: {:.3f}±{:.3f}'.format(
+        steps, train_loss_arr.mean(),train_acc_arr.mean(),train_acc_arr.std(),test_acc_arr.mean(),test_acc_arr.std()))
+
+    # plot your accuracy graph - add a graph like this in your final report
+    plot_data.append([steps, np.array(train_acc_arr).mean(), np.array(train_acc_arr).std(), np.array(test_acc_arr).mean(), np.array(test_acc_arr).std()])
+    reward_list = []
+    plt.plot([x[0] for x in plot_data], [x[1] for x in plot_data], '-', color='tab:grey', label="Train accuracy")
+    plt.fill_between([x[0] for x in plot_data], [x[1]-x[2] for x in plot_data], [x[1]+x[2] for x in plot_data], alpha=0.2, color='tab:grey')
+    plt.plot([x[0] for x in plot_data], [x[3] for x in plot_data], '-', color='tab:purple', label="Test accuracy")
+    plt.fill_between([x[0] for x in plot_data], [x[3]-x[4] for x in plot_data], [x[3]+x[4] for x in plot_data], alpha=0.2, color='tab:purple')
+    plt.xlabel('Steps')
+    plt.ylabel('Accuracy')
+    plt.legend(loc="upper left")
+    plt.show()
+    disp.clear_output(wait=True)
+
+
+
+# %% [markdown]
+# **Inference on dataset**
+# 
+# This is useful for analysis but is entirely optional
+
+# %%
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    img = np.transpose(img, (1, 2, 0))
+    plt.imshow(img)
+
+    predicted_label = np.argmax(predictions_array)
+    color = '#335599' if predicted_label == true_label else '#ee4433'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                  100*np.max(predictions_array),
+                                  class_names[true_label]),
+                                  color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(100), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('#ee4433')
+    thisplot[true_label].set_color('#335599')
+
+test_images, test_labels = next(test_iterator)
+test_images, test_labels = test_images.to(device), test_labels.to(device)
+test_preds = torch.softmax(N(test_images), dim=1).data.cpu().numpy()
+num_rows = 8
+num_cols = 4
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, test_preds, test_labels.cpu().numpy(), test_images.cpu().numpy()) # Used .numpy() here
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, test_preds, test_labels.cpu().numpy())
+
+
